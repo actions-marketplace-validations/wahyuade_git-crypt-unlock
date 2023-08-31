9b8bd7d41edb8c17db720c5bfd850630be84525c
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2023-08-31 22:58:09 +0700

    add ls and pwd

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,9 @@ gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
 
 /usr/libexec/gpg-preset-passphrase --preset --passphrase "$GPG_KEY_PASS" "$GPG_KEY_GRIP"
 
-cd /github/workspace
+ls
+
+pwd
 
 git-crypt unlock
 
